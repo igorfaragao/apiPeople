f33339730dd806763475538cf30d45c62e222605
Igor Felipe Aragão <igorfaragao@gmail.com>
2021-07-15 11:29:15 -0300

    Implementando a funcionalidade de ciração de pessoa

--- src/main/java/com/digitalinnovation/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.digitalinnovation.controller;
 
 import com.digitalinnovation.dto.MessageResponseDTO;
+import com.digitalinnovation.dto.request.PersonDTO;
 import com.digitalinnovation.entity.Person;
 import com.digitalinnovation.repository.PersonRepository;
 import com.digitalinnovation.services.PersonService;
@@ -8,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
     private PersonService personService;
+
     @Autowired
     public PersonController(PersonService personService){
         this.personService = personService;
@@ -21,8 +25,8 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    private MessageResponseDTO createPerson(@RequestBody Person person){
-        return personService.createPerson(person);
+    private MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
 
 }
--- src/main/java/com/digitalinnovation/services/PersonService.java
+++ src/main/java/com/digitalinnovation/services/PersonService.java
@@ -1,17 +1,18 @@
 package com.digitalinnovation.services;
 
 import com.digitalinnovation.dto.MessageResponseDTO;
+import com.digitalinnovation.dto.request.PersonDTO;
 import com.digitalinnovation.entity.Person;
+import com.digitalinnovation.mapper.PersonMapper;
 import com.digitalinnovation.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository){
@@ -20,9 +21,11 @@ public class PersonService {
     }
 
 
-    public MessageResponseDTO createPerson(Person person){
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
 
-        Person savedPerson = personRepository.save(person);
+    Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
 
         return MessageResponseDTO
                 .builder()
